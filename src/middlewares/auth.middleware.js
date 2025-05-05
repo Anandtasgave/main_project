f130c36619f45a8f4366ed575751f899d86533c7
Anandtasgave <tasgaveanand18@gmail.com>
2025-05-05 20:11:30 +0530

    add refreshAccessToken end[oint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,30 +3,29 @@ import { asyncHandler } from '../utils/asyncHandler.js';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user.model.js';
 
-
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
       req.header('Authorization')?.replace('Bearer ', '');
-  
+
     if (!token) {
       throw new ApiError(401, 'unauthorized request');
     }
-  
+
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-  
+
     const user = await User.findById(decodedToken?._id).select(
       '-password -refreshToken'
     );
-  
-    if(!user) {
-      throw new ApiError(401,"Invalid Access Token")
+
+    if (!user) {
+      throw new ApiError(401, 'Invalid Access Token');
     }
-  
+
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(401, error?.message || "Invalid Access Token");
+    throw new ApiError(401, error?.message || 'Invalid Access Token');
   }
 });
